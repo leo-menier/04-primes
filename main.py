5aebe78114e8016ea27d42ca62d886dedc920cc9
leo-menier <leo.menier@edu.esiee.fr>
2024-11-08 12:50:15 +0000

    Exercice qui permet d'obtenir une liste de nombres premiers

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
+"""
+ Permet d'obtenir une liste de nombres premiers.
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
+    """
+    Détermine si un nombre est premier.
 
-    # votre code ici
+    Paramètre:
+    p (int): Le nombre à tester.
 
-    pass
+    Retourne:
+    bool: True si le nombre est premier, False sinon.
+    """
+    # On commence par vérifier si p est strictement inférieur à 2, auquel cas il n'est pas premier
+    if p < 2:
+        return False
+    # On vérifie les diviseurs potentiels de 2 jusqu'à la racine carrée de p
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:  # Si p est divisible par i, ce n'est pas un nombre premier
+            return False
+    # Si aucun diviseur n'est trouvé, p est premier
+    return True
+
 
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Print tous les nombres premiers de 0 à 99.
+    """
+    # Vos appels à la fonction secondaire ici
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
